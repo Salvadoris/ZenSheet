6dba3ff57fafaf62f2c06e3a9e13d0b67377826d
Clashy13 <s.kiste@web.de>
2026-01-17 21:41:20 +0100

    remove forced path method from drawing

--- Frontend/src/app/layout/canvas/Drawings/Drawing.ts
+++ Frontend/src/app/layout/canvas/Drawings/Drawing.ts
@@ -8,7 +8,6 @@ import { Shape } from '../Shapes/Shape';
 export interface Drawing {
   properties: DrawingProperties;
   update(p: Point): ChangableDrawingProperties;
-  path(): Path2D;
   render(ctx: CanvasRenderingContext2D): void;
   toShape(ctx: CanvasRenderingContext2D): Shape;
 }
